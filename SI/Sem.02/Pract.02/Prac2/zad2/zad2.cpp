97dfe5b4468bdd819843129b317076857afc0983
Silvia <zemedelska148@gmail.com>
2022-10-12 20:01:22 +0300

    Task 02
    Created solution for task 02 - 12.10.22

---
+++ SI/Sem.02/Pract.02/Prac2/zad2/zad2.cpp
@@ -0,0 +1,36 @@
+﻿// zad2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    char letter;
+    std::cin >> letter;
+    if ((letter >= 'a' && letter <= 'z') || (letter >= 'A' && letter <= 'Z'))
+    {
+        switch (letter)
+        {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'u':
+        case 'o':
+        case 'A':
+        case 'E':
+        case 'I':
+        case 'U':
+        case 'O':
+            std::cout << "гласна";
+            break;
+
+        default:
+            std::cout << "съгласна";
+            break;
+        }
+    }
+    else {
+        std::cout << "некоректен вход";
+    }
+
+}
