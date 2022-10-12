db23e05cfd36993ebd107edd5cf911f84bee78b7
Silvia <zemedelska148@gmail.com>
2022-10-12 20:00:44 +0300

    Task 01
    solution for task 1 1- 2.10.22

---
+++ SI/Sem.02/Pract.02/Prac2/Prac2/Prac2.cpp
@@ -0,0 +1,15 @@
+﻿// zad1
+
+#include <iostream>
+
+int main()
+{
+    int year = 0;
+    std::cin >> year;
+    if (year % 4 == 0) {
+        std::cout << "Is leap year";
+    }
+    else {
+        std::cout << "Is NOT leap year";
+    }
+}
