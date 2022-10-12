63eecc84d37dec65eddbddc243d4d0fd1fbb1910
Silvia <zemedelska148@gmail.com>
2022-10-12 20:03:44 +0300

    Task 03
    Created solution for task 03 - 12.10.22

---
+++ SI/Sem.02/Pract.02/Prac2/zad3/zad3.cpp
@@ -0,0 +1,12 @@
+﻿// zad3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    int a = 0, b = 0;
+    std::cin >> a >> b;
+    int result = a > b ? a : b;
+    std::cout << "Bigger number is: " << result;
+}
