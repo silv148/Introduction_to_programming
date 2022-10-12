92d7a6677ece1c5135b93c588a79d643f4c05760
Silvia <zemedelska148@gmail.com>
2022-10-12 20:04:49 +0300

    Task 04
    Created solution for task 04 - 12.10.22

---
+++ SI/Sem.02/Pract.02/Prac2/Task_04/Task_04.cpp
@@ -0,0 +1,29 @@
+﻿// Task_04.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    int percentage = 0;
+    std::cout << "Result: ";
+    std::cin >> percentage;
+    if (percentage >= 90) {
+        std::cout << "Grade: 6++";
+    }
+    else if (percentage >= 80) {
+        std::cout << "Grade: 6";
+    }
+    else if (percentage >= 70) {
+        std::cout << "Grade: 5";
+    }
+    else if (percentage >= 60) {
+        std::cout << "Grade: 4";
+    }
+    else if (percentage >= 40) {
+        std::cout << "Grade: 3";
+    }
+    else {
+        std::cout << "Grade: 2";
+    }
+}
